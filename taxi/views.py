d8c9dbf0080877acee2e26a7d199e043c97bb41c
Anton Hryhorenko <anton.h.sind@gmail.com>
2024-08-11 21:46:00 +0200

    fixed queryset

--- taxi/views.py
+++ taxi/views.py
@@ -39,4 +39,4 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.prefetch_related("cars")
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
